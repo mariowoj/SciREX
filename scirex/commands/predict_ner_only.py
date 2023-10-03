b641fe6f2c8a8433b7181126adf248ef6be5a26a
mariowoj <supermario.woj@googlemail.com>
2023-10-03 13:53:37 +0200

    Adapt new metrics in original scirewx

---
+++ scirex/commands/predict_ner_only.py
@@ -0,0 +1,18 @@
+# https://github.com/viswavi/SciREX
+export test_file=scirex_dataset/release_data/test.jsonl
+export test_output_folder=../drive/MyDrive/SciREX/test_outputs/ner_only/
+export ner_output_folder=../drive/MyDrive/SciREX/test_outputs/ner_only/
+export scirex_archive=../drive/MyDrive/original/a100_b20_e20/outputs/pwc_outputs/experiment_scirex_full/main/
+export cuda_device=0
+
+echo "Predicting NER"
+python scirex/predictors/predict_ner.py \
+$scirex_archive \
+$test_file \
+$ner_output_folder/ner_predictions.jsonl \
+$cuda_device
+
+echo "Evaluating on NER only"
+python scirex/evaluation_scripts/ner_evaluate.py \
+--gold-file $test_file \
+--ner-file $ner_output_folder/ner_predictions.jsonl
---
+++ scirex/commands/predict_salient_only_gold.sh
@@ -0,0 +1,26 @@
+# https://github.com/viswavi/SciREX
+export test_file=scirex_dataset/release_data/test.jsonl
+export test_output_folder=../drive/MyDrive/SciREX/test_outputs/salient_only/
+export scirex_archive=../drive/MyDrive/SciREX/original/a100_b20_e20/outputs/pwc_outputs/experiment_scirex_full/main/
+export cuda_device=0
+
+
+echo "Predicting Salient Mentions"
+python scirex/predictors/predict_salient_mentions.py \
+$scirex_archive \
+scirex_dataset/release_data/test.jsonl \
+$test_output_folder/salient_mentions_predictions.jsonl \
+$cuda_device
+
+echo "Predicting Salient Clustering "
+python scirex/predictors/predict_salient_clusters.py \
+$test_file \
+$test_output_folder/salient_mentions_predictions.jsonl \
+$test_output_folder/salient_clusters_predictions.jsonl
+
+echo "Evaluating on all gold steps "
+python scirex/evaluation_scripts/salient_only_evaluate.py \
+--gold-file $test_file \
+--ner-file $test_file \
+--clusters-file $test_output_folder/salient_clusters_predictions.jsonl \
+--salient-mentions-file $test_output_folder/salient_mentions_predictions.jsonl
